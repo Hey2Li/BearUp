c078eca62bbbc5196c53e9402eba13e99e4be7c1
Lee2Go <hey2li@163.com>
2016-10-25 16:47:32 +0800

    网络请求

--- BearUp/BearUp/LHTTPManager.h
+++ BearUp/BearUp/LHTTPManager.h
@@ -7,8 +7,10 @@
 //
 
 #import "LTHTTPSessionManager.h"
-
+#define kRequestHomeArticle @"http://static.owspace.com/index.php?m=Home&c=Api&a=getList&p=1&client=iphone&page_id=0&create_time=0&iOS_version=1.0.0"
 @interface LHTTPManager : LTHTTPSessionManager
 
 + (void)GETRequsetTableWithModel:(NSString *)model Page:(NSString *)page Page_id:(NSString *)page_id Create_time:(NSString *)create_time Complete:(completeBlock)complete;
+
++ (void)GetRequestHomeArticle:(completeBlock)complete;
 @end
--- BearUp/BearUp/LHTTPManager.m
+++ BearUp/BearUp/LHTTPManager.m
@@ -13,4 +13,9 @@
     LTHTTPSessionManager *manager = [LTHTTPSessionManager new];
     [manager GETWithParameters:[NSString stringWithFormat:@"http://static.owspace.com/index.php?m=Home&c=Api&a=getList&model=%@&p=%@&client=iphone&page_id=%@&create_time=%@&iOS_version=1.0.0",model,page,page_id,create_time] parameters:nil complete:complete];
 }
+
++ (void)GetRequestHomeArticle:(completeBlock)complete{
+    LTHTTPSessionManager *manager = [LTHTTPSessionManager new];
+    [manager GETWithParameters:[NSString stringWithFormat:@"http://static.owspace.com/index.php?m=Home&c=Api&a=getList&p=3&client=iphone&page_id=0&create_time=0&iOS_version=1.0.0"] parameters:nil complete:complete];
+}
 @end
